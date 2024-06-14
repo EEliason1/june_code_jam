c8f8b0a87d11b3cba514bbb05f7a4de1ec77cece
RosaYi <rosayihuilin@gmail.com>
2024-06-14 16:06:52 -0400

    add Linkedin info

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+const linkedinProfileLink = [
+  {
+    name: "Dan Pocock",
+    image: "../images/linkedin_profile/Dan_Pocock.jpg",
+    link: "https://www.linkedin.com/in/danpocock/",
+  },
+  {
+    name: "Mike Velasco",
+    image: "../images/linkedin_profile/Mike_Velasco",
+    link: "https://www.linkedin.com/in/mike-velasco-148b5a50/",
+  },
+  {
+    name: "Evan Eliason",
+    image: "../images/linkedin_profile/Evan_Elison.jpg",
+    link: "https://www.linkedin.com/in/evan-eliason/",
+  },
+  {
+    name: "Yi Hui Lin",
+    image: "../images/linkedin_profile/Yi_Hui_Lin.jpg",
+    link: "www.linkedin.com/in/yihuilinrosa",
+  },
+];
